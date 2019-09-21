d9c8bd1ab94ca8e953fad8e57c832f45579e8d05
Renat <suleymanov_renat@mail.ru>
2019-09-21 22:07:24 +0300

    tasks 1-3 in variables

---
+++ variables/main.js
@@ -0,0 +1,16 @@
+let algebra = 5;
+let gepgraphy = 5;
+let physics = 4;
+let average = (algebra + gepgraphy + physics) / 3;
+console.log(average);
+
+let name = "Ренат";
+let message = `Привет, Мир! Меня зовут ${name}`;
+console.log(message);
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = (x * y) + (5 * z) + (x - 1);
+console.log(result);
+
